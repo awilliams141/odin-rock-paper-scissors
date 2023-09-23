1f0b9e5956b1c7e1586a0c2803226945cd58bd42
awilliams141 <austinjwilliams141@gmail.com>
2023-09-22 22:19:11 -0400

    Added the playRound function

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
 
     if (num === 1) {
-        return "Rock";
+        return "rock";
     } else if (num === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    
+    //console.log("Make your choice! Rock, Paper, or scissors?");
+    //playerSelection = prompt();
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper covers rock!";
+        } else if (computerSelection === "scissors") {
+            return "You win! Rock crushes scissosrs!";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors cuts paper!";
+        } else if (computerSelection === "rock") {
+            return "You win! Paper covers rock!";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock crushes scissors!";
+        } else if (computerSelection === "paper") {
+            return "You win! Scissors cuts paper!";
+        }
+    }
+    return "It's a tie! You both chose the same!"
 }
 
