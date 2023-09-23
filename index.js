fe3338c727841a1bece51e2bfe569d579ef73b1f
awilliams141 <austinjwilliams141@gmail.com>
2023-09-22 21:47:21 -0400

    Created intial code files and added computer choice method

---
+++ index.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const num = Math.floor(Math.random() * 3) + 1;
+
+    if (num === 1) {
+        return "Rock";
+    } else if (num === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
